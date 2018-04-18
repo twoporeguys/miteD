3fdfc80ff2582ce1a72222586d79da4dbddcb59b
Pierre Chaussalet <pierre.chaussalet@twoporeguys.com>
2018-04-17 17:40:04 -0700

    [middleware] Support method decorators in any order

--- miteD/middleware/api.py
+++ miteD/middleware/api.py
@@ -4,30 +4,14 @@ from contextlib import suppress
 from nats.aio.client import Client as NATS
 from sanic import Sanic, response
 
+from miteD.service.errors import MiteDRPCError
 from miteD.service.client import RemoteService
-
-
-def parse_wrapped_endpoints(wrapped):
-    endpoints = {'*': {}}
-    for member in [getattr(wrapped, member_name) for member_name in dir(wrapped)]:
-        if callable(member):
-            if hasattr(member, '__api_path__') \
-                    and hasattr(member, '__api_method__') \
-                    and hasattr(member, '__api_versions__'):
-                for api_version in member.__api_versions__:
-                    version = endpoints.get(api_version, {})
-                    path = version.get(member.__api_path__, {})
-                    path[member.__api_method__] = member
-                    version[member.__api_path__] = path
-                    endpoints[api_version] = version
-    return endpoints
 
 
 def api(name, versions, broker_urls=('nats://127.0.0.1:4222',)):
     def wrapper(cls):
 
         class Api(object):
-            _app = Sanic(name=name)
             _loop = asyncio.get_event_loop()
             _broker_urls = broker_urls
             _nc = NATS()
@@ -35,20 +19,12 @@ def api(name, versions, broker_urls=('nats://127.0.0.1:4222',)):
             def __init__(self):
                 cls.loop = self._loop
                 cls.get_remote_service = self.get_remote_service
-                wrapped = cls()
-                self.endpoints = parse_wrapped_endpoints(wrapped)
-                for version in versions:
-                    for path, methods in self.endpoints.get('*', {}).items():
-                        for method, handler in methods.items():
-                            self._app.add_route(handler, '/' + version + path, methods=[method])
-                    for path, methods in self.endpoints.get(version, {}).items():
-                        for method, handler in methods.items():
-                            self._app.add_route(handler, '/' + version + path, methods=[method])
 
             async def _connect(self):
                 return await self._nc.connect(io_loop=self._loop, servers=self._broker_urls, verbose=True)
 
             def start(self):
+                self._load_app()
                 print('\n'.join(['{} {}'.format(*(list(route.methods)[0], path))
                                  for path, route in self._app.router.routes_all.items()]))
                 server = self._app.create_server(host='0.0.0.0', port=8000)
@@ -67,6 +43,52 @@ def api(name, versions, broker_urls=('nats://127.0.0.1:4222',)):
 
             def get_remote_service(self, service_name, version):
                 return RemoteService(name=service_name, version=version, nc=self._nc)
+
+            def _load_app(self):
+                self.endpoints = {'*': {}}
+                self._app = Sanic(name=name)
+                self.parse_wrapped_endpoints()
+                for version in versions:
+                    for path, methods in self.endpoints.get('*', {}).items():
+                        for method, handler in methods.items():
+                            self._app.add_route(handler, '/' + version + path, methods=[method])
+                    for path, methods in self.endpoints.get(version, {}).items():
+                        for method, handler in methods.items():
+                            self._app.add_route(handler, '/' + version + path, methods=[method])
+
+            def parse_wrapped_endpoints(self):
+                wrapped = cls()
+                for member in [getattr(wrapped, member_name) for member_name in dir(wrapped)]:
+                    if callable(member):
+                        if hasattr(member, '__api_path__') \
+                                and hasattr(member, '__api_method__') \
+                                and hasattr(member, '__api_versions__'):
+                            for api_version in member.__api_versions__:
+                                version = self.endpoints.get(api_version, {})
+                                path = version.get(member.__api_path__, {})
+                                path[member.__api_method__] = self._type_response(member)
+                                version[member.__api_path__] = path
+                                self.endpoints[api_version] = version
+
+            @staticmethod
+            def _type_response(method):
+                response_type = method.__response_type__ if hasattr(method, '__response_type__') else response.json
+
+                async def typed_handler(*args, **kwargs):
+                    result = method(*args, **kwargs)
+                    try:
+                        result = (await result) if asyncio.iscoroutine(result) else result
+                        print(result)
+                        if isinstance(result, tuple):
+                            body = result[0]
+                            rest = result[1:]
+                        else:
+                            body = result
+                            rest = ()
+                        return response_type(*(body, *rest))
+                    except MiteDRPCError as err:
+                        return err.message, (err.status,)
+                return typed_handler
 
         return Api
 
--- miteD/middleware/types.py
+++ miteD/middleware/types.py
@@ -1,43 +1,22 @@
-from asyncio import iscoroutine
-
 from sanic import response
-
-from miteD.service.errors import MiteDRPCError
-
-
-async def _build_response(result):
-    try:
-        result = (await result) if iscoroutine(result) else result
-        if isinstance(result, tuple):
-            body = result[0]
-            rest = result[1:]
-        else:
-            body = result
-            rest = ()
-        return body, rest
-    except MiteDRPCError as err:
-        return err.message, (err.status,)
 
 
 def json(fn):
-    async def wrapper(*args, **kwargs):
-        body, rest = await _build_response(fn(*args, **kwargs))
-        return response.json(*(body, *rest))
-
-    return wrapper
+    fn.__response_type__ = response.json
+    return fn
 
 
 def text(fn):
-    async def wrapper(*args, **kwargs):
-        body, rest = await _build_response(fn(*args, **kwargs))
-        return response.text(*(body, *rest))
-
-    return wrapper
+    fn.__response_type__ = response.text
+    return fn
 
 
 def raw(fn):
-    async def wrapper(*args, **kwargs):
-        body, rest = await _build_response(fn(*args, **kwargs))
-        return response.raw(*(body, *rest))
+    fn.__response_type__ = response.raw
+    return fn
 
-    return wrapper
+
+def html(fn):
+    fn.__response_type__ = response.html
+    return fn
+
