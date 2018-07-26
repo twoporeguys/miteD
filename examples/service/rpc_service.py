932a246790c4b9d2147f122d5489b71ca7a3cc0a
Piotr Glinianowicz <p.glinianowicz.mobile@gmail.com>
2018-07-26 20:27:34 +0200

    Make service example worky again

--- examples/service/rpc_service.py
+++ examples/service/rpc_service.py
@@ -1,14 +1,14 @@
 from miteD.service.service import rpc_service, rpc_method
 
 
-@rpc_service(name='test', versions=['1.0', '1.1'], broker_urls=['nats://nats:4222/'])
+@rpc_service(name='test', versions=['1.0', '1.1'], broker_urls=['nats://127.0.0.1:4222/'])
 class RpcService:
     @rpc_method(versions=['1.0'])
-    def ping(self, *args):
+    async def ping(self, *args):
         return 'pong'
 
     @rpc_method(name='ping', versions=['1.1'])
-    def ping_1_1(self, *args):
+    async def ping_1_1(self, *args):
         return 'Pong'
 
 
--- examples/service/rpc_service2.0.py
+++ examples/service/rpc_service2.0.py
@@ -4,15 +4,15 @@ from miteD.service.service import rpc_service, rpc_method
 @rpc_service(name='test', versions=['2.0'], broker_urls=['nats://127.0.0.1:4222'])
 class RpcService:
     @rpc_method(versions=['2.0'])
-    def ping(self, *args):
+    async def ping(self, *args):
         return 'pong 2.0'
 
     @rpc_method()
-    def hello(self, name):
+    async def hello(self, name):
         return 'Hello ' + name
 
     @rpc_method(name='add')
-    def addition(self, x, y):
+    async def addition(self, x, y):
         print(x)
         return x + y
 
