a2ded57ccff73ca5f8d221d07c68e0deebee6268
Piotr Glinianowicz <p.glinianowicz.mobile@gmail.com>
2018-08-02 18:38:11 +0200

    Added notifications usage examples

---
+++ examples/service/notifications_consumer.py
@@ -0,0 +1,49 @@
+import logging
+
+from miteD.service.service import rpc_service, notification_handler
+
+
+@rpc_service(
+    name='notifications_consumer',
+    versions=['1.0'],
+    broker_urls=['nats://127.0.0.1:4222'],
+)
+class NotificationsConsumer:
+    @notification_handler(
+        layer='service',
+        name='notifications_producer',
+        versions=['1.0'],
+        topic='updates',
+    )
+    async def updates_handler(self, channel, msg):
+        logging.debug("updates_handler: got notification from channel: {} msg = '{}'".format(channel, msg))
+
+    @notification_handler(
+        layer='service',
+        name='notifications_producer',
+        versions=['1.0'],
+        topic='errors',
+    )
+    async def errors_handler(self, channel, msg):
+        logging.debug("errors_handler: got notification from channel: {} msg = '{}'".format(channel, msg))
+
+    @notification_handler(
+        layer='*',
+        name='*',
+        versions=['*'],
+        topic='updates',
+    )
+    async def wildcard_handler(self, channel, msg):
+        logging.debug("wildcard_updates_handler: got notification from channel: {} msg = '{}'".format(channel, msg))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    service = NotificationsConsumer()
+
+    try:
+        logging.info("Starting NotificationsConsumer")
+        service.start()
+    except KeyboardInterrupt:
+        logging.info("Stopping NotificationsConsumer")
+        service.stop()
