80d99a4b95a5e9bcb3f37a8408b91d5eeb3f0956
Suraj Ravichandran <surajravi@gmail.com>
2018-11-19 13:35:41 -0800

    Bump version to 2.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miteD',
-    version='2.0.0',
+    version='2.1.0',
     packages=find_packages(exclude=['examples']),
     description='Api and service infrastructure library for X1 (based on sanic and nats)',
     url='https://github.com/twoporeguys/miteD/',
